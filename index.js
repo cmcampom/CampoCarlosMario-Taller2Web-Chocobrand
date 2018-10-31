6c5a91ff19e131d2dac968655236d8f3961bf470
cmcampom <lewiscampo@gmail.com>
2018-10-30 23:39:19 -0500

    Ajustes finales para hora limite

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ client.connect(function(err){
 app.get('/', function(request, response){
 
     const collection = db.collection('muffins');
+    
 
     collection.find({}).toArray(function(err,docs){
 
@@ -42,15 +43,7 @@ app.get('/', function(request, response){
 
         var contexto = {
             products: docs,
-            nombre : "NombrePrueba",
-            precio : "100",
-            especial : false,
-            gluten : true,
-            dulce: 60,
-            fav: false,
-            car: false,
-            img: "/public//img//rubySunset.png"
-        };
+            };
     
         response.render('ultcolec',contexto);
 
@@ -58,6 +51,185 @@ app.get('/', function(request, response){
 
     })
 });
+
+app.get('/OnlyRealShXt', function(request, response){
+
+    const collection = db.collection('muffins');
+    
+
+    collection.find({
+
+        especial: true
+
+    }).toArray(function(err,docs){
+
+        if(err){
+            console.error(err);
+            response.send(err);
+            return;
+        };
+
+        var contexto = {
+            products: docs,
+            };
+    
+        response.render('ultcolec',contexto);
+
+
+
+    })
+});
+
+app.get('/Sweet/Low', function(request, response){
+
+    const collection = db.collection('muffins');
+    
+
+    collection.find({
+
+        dulce: {
+            '$lt': 31
+        }
+
+    }).toArray(function(err,docs){
+
+        if(err){
+            console.error(err);
+            response.send(err);
+            return;
+        };
+
+        var contexto = {
+            products: docs,
+            };
+    
+        response.render('ultcolec',contexto);
+
+
+
+    })
+});
+
+app.get('/Sweet/Medium', function(request, response){
+
+    const collection = db.collection('muffins');
+    
+
+    collection.find({
+
+        dulce: {
+            '$lt': 61,
+            '$gt': 31
+        }
+
+    }).toArray(function(err,docs){
+
+        if(err){
+            console.error(err);
+            response.send(err);
+            return;
+        };
+
+        var contexto = {
+            products: docs,
+            };
+    
+        response.render('ultcolec',contexto);
+
+
+
+    })
+});
+
+app.get('/Sweet/High', function(request, response){
+
+    const collection = db.collection('muffins');
+    
+
+    collection.find({
+
+        dulce: {
+            '$gt': 61
+        }
+
+    }).toArray(function(err,docs){
+
+        if(err){
+            console.error(err);
+            response.send(err);
+            return;
+        };
+
+        var contexto = {
+            products: docs,
+            };
+    
+        response.render('ultcolec',contexto);
+
+
+
+    })
+});
+
+app.get('/GlutenFree', function(request, response){
+
+    const collection = db.collection('muffins');
+    
+
+    collection.find({
+
+        'gluten': false
+
+    }).toArray(function(err,docs){
+
+        if(err){
+            console.error(err);
+            response.send(err);
+            return;
+        };
+
+        var contexto = {
+            products: docs,
+            };
+    
+        response.render('ultcolec',contexto);
+
+
+
+    })
+});
+
+app.get('/MyFavs', function(request, response){
+
+    const collection = db.collection('muffins');
+    
+
+    collection.find({
+
+        'fav': true
+
+    }).toArray(function(err,docs){
+
+        if(err){
+            console.error(err);
+            response.send(err);
+            return;
+        };
+
+        var contexto = {
+            products: docs,
+            };
+    
+        response.render('ultcolec',contexto);
+
+
+
+    })
+});
+
+
+
+
 
 
 app.listen(5500,function(){
